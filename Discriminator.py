2ba080093655d51acf777384aea8ac31f831f58c
Skyler Maxwell <maxwels2@tcnj.edu>
2019-03-26 15:03:04 -0400

    WIP:

--- Discriminator.py
+++ Discriminator.py
@@ -15,21 +15,21 @@ class Discriminator:
         
         # Fix input_shape later
 
-        self.arch.add(Conv2D(32, 3, padding='same', strides=2, input_shape=(256, 256, 1)))
+        self.arch.add(Conv2D(32, 3, padding='same', strides=2, input_shape=(256, 256, 3), data_format='channels_last'))
         self.arch.add(LeakyReLU(0.2))
-        self.arch.add(Dropout(0.4))
+        self.arch.add(Dropout(0.1))
 
-        self.arch.add(Conv2D(64, 3, padding='same', strides=1))
+        self.arch.add(Conv2D(64, 3, padding='same', strides=2, data_format='channels_last'))
         self.arch.add(LeakyReLU(0.2))
-        self.arch.add(Dropout(0.4))
+        self.arch.add(Dropout(0.1))
 
-        self.arch.add(Conv2D(128, 3, padding='same', strides=2))
+        self.arch.add(Conv2D(128, 3, padding='same', strides=2, data_format='channels_last'))
         self.arch.add(LeakyReLU(0.2))
-        self.arch.add(Dropout(0.4))
+        self.arch.add(Dropout(0.1))
 
-        self.arch.add(Conv2D(256, 3, padding='same', strides=1))
+        self.arch.add(Conv2D(256, 3, padding='same', strides=2, data_format='channels_last'))
         self.arch.add(LeakyReLU(0.2))
-        self.arch.add(Dropout(0.4))
+        self.arch.add(Dropout(0.1))
 
         self.arch.add(Flatten())
 
@@ -39,8 +39,8 @@ class Discriminator:
         adam_lr = 0.0002
         adam_beta_1 = 0.5
         
-        self.arch.compile(optimizer=Adam(lr=adam_lr, beta_1=adam_beta_1),
-                    loss='binary_crossentropy',
+        self.arch.compile(optimizer='adam',
+                    loss='categorical_crossentropy',
                     metrics=['accuracy'])
         
         return
@@ -50,8 +50,10 @@ class Discriminator:
     
     def fit_data(self, train_data, train_labels, test_data, test_labels):
         train_history = self.arch.fit(train_data, train_labels,
-                                      epochs=5, steps_per_epoch=1000, validation_steps=200,
-                                      validation_data=(test_data, test_labels))
+                                      epochs=5, steps_per_epoch=train_data.shape[0], validation_steps=test_data.shape[0],
+                                      validation_data=(test_data, test_labels),
+                                      shuffle=True
+                                      )
         return train_history
 
 
--- MobileNetModel.py
+++ MobileNetModel.py
@@ -1,41 +1,41 @@
 from keras.models import Model, load_model
 from keras.applications.mobilenet import MobileNet, preprocess_input
 
-from ClassificationModel import ClassificationModel
+#from ClassificationModel import ClassificationModel
 
-class MobileNetModel(ClassificationModel):
+class MobileNetModel:#(ClassificationModel):
     def __init__(self):
-        super(CardModel, self).__init___(model_name='CardModel')
-        self.num_classes = 13
+        #super(MobileNetModel, self).__init___(model_name='MobileNet')
+        self.num_classes = 2
         self.build_model()
 
         return
 
     def build_model(self):
         # Initializing the model with random wights
-        self.model = MobileNet(weights=None, include_top=True, classes=self.num_classes)
-
-        print(self.model.summary())
+        self.arch = MobileNet(weights=None, input_shape=(256,256,3), classes=self.num_classes)
 
         # Compiling model with optimization of RSM and cross entropy loss
-        self.model.compile(optimizer='rmsprop',
+        self.arch.compile(optimizer='rmsprop',
               loss='categorical_crossentropy',
               metrics=['accuracy'])
         return
 
-    def train(self, epochs, train_images, train_labels, val_data, batch_size, initial_epoch=None):
-        history = self.model.fit_generator(
-            train_generator,
-            epochs=epochs,
-            steps_per_epoch=train_generator.samples // batch_size,
-            validation_data=validation_generator,
-            validation_steps=validation_generator.samples // batch_size)
-        return history
+    def __repr__(self):
+        return str(self.arch.summary())
+
+    def fit_data(self, train_images, train_labels, val_images, val_labels, initial_epoch=None):
+        train_history = self.arch.fit(train_images, train_labels,
+                                      epochs=5, steps_per_epoch=train_images.shape[0], validation_steps=val_images.shape[0],
+                                      validation_data=(val_images, val_labels),
+                                      shuffle=True
+                                      )
+        return train_history
 
     def save_model(self, model_path):
-        self.model.save(model_path)
+        self.arch.save(model_path)
         return
 
     def load_model(self, model_path):
-        self.model = load_model(model_path)
+        self.arch = load_model(model_path)
         return
